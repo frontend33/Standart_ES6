7458ea26407a0c31106d61f0661a9c0530c71ee9
frontend33 <vikulov33@rambler.ru>
2018-02-25 21:05:52 +0300

    Задаем параметр по умолчанию,спред оператор троеочия, новый цикл of

--- es6/scripts.js
+++ es6/scripts.js
@@ -1,30 +1,32 @@
 "use strict"
-
-// window.onload=function(e){
-// 	for (var i=0;i<5;i++){
-// 		var a=33;
-// 		// console.log(i);
-// 	};
-// 	console.log(i);
-// 	console.log(a);
-// };
-
-
-
-// // let and const
-// // let улучшенный var (Нормальные зоны видимости элементов)
-// window.onload=function(e){
-// 	for (let i=0;i<5;i++){
-// 		console.log(i);
-// 	};
+/*
+window.onload=function(e){
+	for (var i=0;i<5;i++){
+		var a=33;
+		// console.log(i);
+	};
+	console.log(i);
+	console.log(a);
+};
+*/
 
 
-// 	// Сдесь не сработает так как переменная заданная внутри фигурных скобок не вырвется
-// 	// console.log(i);
-// 	// console.log(a);
-// };
+/*
+
+// let and const
+// let улучшенный var (Нормальные зоны видимости элементов)
+window.onload=function(e){
+	for (let i=0;i<5;i++){
+		console.log(i);
+	};
 
 
+	// Сдесь не сработает так как переменная заданная внутри фигурных скобок не вырвется
+	// console.log(i);
+	// console.log(a);
+};
+
+*/
 
 window.onload=function(e){
 	const test=50;
@@ -37,8 +39,8 @@ window.onload=function(e){
 	}
 
 	// settings= {a:5}; выведет ошибку
- // Но не смотря на то что settings константа ее составная часть может изменяться
- settings.a=15;
+
+ settings.a=15; // Но не смотря на то что settings константа ее составная часть может изменяться
  settings.d='Hello';
 
 
@@ -48,7 +50,73 @@ window.onload=function(e){
 	};
 	console.log(test);
 	console.log(settings);
-	// Сдесь не сработает так как переменная заданная внутри фигурных скобок не вырвется
-	// console.log(i);
+
+
+	
+	// console.log(i);// Сдесь не сработает так как переменная заданная внутри фигурных скобок не вырвется
 	// console.log(a);
 };
+
+
+/* 
+
+// Раньше мы прописывали t которой передавали значение из переменной 
+// и если такой переменной нет мы через оператор или присваивали ей какое то значение
+timer(5);
+timer();
+timer(0);//let time=t || 60 при таком обозначении
+// Выведет 60
+
+// Выведет в первом логе 5 во втором 60 в третьем 60 при форме let time=t || 60
+function timer(t){
+	// let time=t || 60; //Не универсальная форма записи 
+
+	let time=(typeof t==='undefined') ? 60 :t; 	// Такой костыль используем и выведет 0
+	console.log(time);
+};
+*/
+
+
+
+// В стандарте ES6 используется еще проще
+timer(5);
+timer();
+timer(0);
+
+function timer(time=60){
+	console.log(time)
+};
+
+/*
+some('Petr',[5,5,5,4,5]);
+function some(name,marks){
+	console.log(arguments)
+	};
+	*/
+
+let str=some('Petr','Ruchkin',5,3,4);
+document.querySelector('.res').innerHTML=str
+//Если мы хотим получить и последующие значения массива, но не уверены в их числе – можно добавить ещё один параметр
+ // , который получит «всё остальное»,при помощи оператора "..." («spread», троеточие):
+
+function some(name,lastname,...marks){
+
+	// console.log(name);
+	// console.log(marks);
+	// Раньше писали marks[m] ключ значения (что бы вывести не по индексу)
+	for(let m in marks){
+		console.log('______');
+		console.log(marks[m]);
+	};
+
+// Теперь есть новый цикл of
+	let sum=0
+	for(let m of marks){
+		sum+=m
+		console.log('______');
+		console.log(m);
+	}
+	let avg=sum/marks.length
+	let res='<div>'+name+' '+ lastname+ '<strong>'+' '+avg+'</strong></div>'
+	return res
+	};
